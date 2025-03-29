c1c56f779d8bf9696e7477fc53c91c73a275d0bc
thuongnv2 <thuongnv2@sapo.vn>
2025-03-29 11:18:39 +0700

    Done stage 2

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,44 +8,51 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argc, char **argv) {
+int CreateSocket() {
+    const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        throw std::runtime_error("Failed to create server socket");
+    }
 
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
+    constexpr int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        throw std::runtime_error("set sock_opt failed");
+    }
 
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "set sock_opt failed\n";
-    return 1;
-  }
-  
-  sockaddr_in server_addr{};
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
-  
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  sockaddr_in client_addr{};
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(6379);
 
-  accept(server_fd, reinterpret_cast<sockaddr *>(&client_addr), (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+    if (bind(server_fd, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) != 0) {
+        throw std::runtime_error("Failed to bind to port 6379");
+    }
 
-  close(server_fd);
+    if (listen(server_fd, 5) != 0) {
+        throw std::runtime_error("listen failed");
+    }
 
-  return 0;
+    std::cout << "Waiting for a client to connect...\n";
+
+    return server_fd;
+}
+
+
+int main() {
+    const int server_fd = CreateSocket();
+
+    sockaddr_in client_addr{};
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    const int client_fd = accept(server_fd, reinterpret_cast<sockaddr *>(&client_addr), &client_addr_len);
+
+    std::cout << "Client connected\n";
+
+    const std::string response = "+PONG\r\n";
+
+    write(client_fd, response.c_str(), response.length());
+
+    close(server_fd);
+
+    return 0;
 }
