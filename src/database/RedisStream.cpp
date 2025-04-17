7e3a6a36b2ed21c20dfdfc9457e0f4aa0c30f318
thuongnv2 <thuongnv2@sapo.vn>
2025-04-17 20:02:33 +0700

    Validating entry IDs #hq8

--- src/database/RedisStream.cpp
+++ src/database/RedisStream.cpp
@@ -1,30 +1,53 @@
 #include "RedisStream.h"
 #include "../parser/RespParser.h"
+#include "../utils/Utils.h"
 
 std::string RedisStream::Add(const std::string &stream_key,
                              const std::string &entry_id,
                              const std::string &key,
                              const std::string &value) {
-    if (Exists(stream_key, entry_id)) {
+
+    const auto pos = entry_id.find('-');
+    if (pos == std::string::npos) {
+        return RespParser::ToError("ERR wrong format of entry_id");
+    }
+
+    const std::string timestamp_str = entry_id.substr(0, pos);
+    const std::string sequence_str = entry_id.substr(pos + 1);
+
+    if (!Utils::IsNumeric(timestamp_str) || !Utils::IsNumeric(sequence_str)) {
+        return RespParser::ToError("ERR wrong number of arguments for 'xadd' command");
+    }
+
+    const int64_t new_timestamp = std::stoll(timestamp_str);
+    const int64_t new_sequence = std::stoll(sequence_str);
+
+    if (new_timestamp == 0 && new_sequence == 0) {
+        return RespParser::ToError("ERR The ID specified in XADD must be greater than 0-0");
+    }
+
+    auto add_stream = [&]()-> std::string {
+        const StreamEntry new_entry{new_timestamp, new_sequence, key, value};
+        stream_entries_[stream_key].push_back(new_entry);
+        return RespParser::ToBulkString(entry_id);
+    };
+
+    if (!stream_entries_.contains(stream_key)) {
+        return add_stream();
+    }
+
+    const std::vector<StreamEntry> &entries = stream_entries_[stream_key];
+    const StreamEntry last_entry = entries.back();
+
+    const bool is_valid_sequence = new_timestamp > last_entry.timestamp
+                                   || (new_timestamp == last_entry.timestamp && new_sequence > last_entry.sequence);
+
+    if (!is_valid_sequence) {
         return RespParser::ToError("ERR The ID specified in XADD is equal or smaller than the target stream top item");
     }
-
-    const StreamEntry new_entry{entry_id, key, value};
-    stream_entries_[stream_key] = new_entry;
-
-    return RespParser::ToBulkString(entry_id);
-}
-
-bool RedisStream::Exists(const std::string &stream_key, const std::string &entry_id) {
-    if (stream_entries_.contains(stream_key)) {
-        return false;
-    }
-    const StreamEntry stream_entry = stream_entries_[stream_key];
-
-    return stream_entry.entry_id == entry_id;
+    return add_stream();
 }
 
 bool RedisStream::ExistStreamKey(const std::string &stream_key) const {
     return stream_entries_.contains(stream_key);
 }
-
--- src/database/RedisStream.h
+++ src/database/RedisStream.h
@@ -4,45 +4,53 @@
 #include <utility>
 #include <vector>
 #include <unordered_map>
+#include <stack>
 
 struct FieldValuePair {
     std::string key;
     std::string value;
 
     FieldValuePair(std::string key, std::string value) noexcept
-        : key(std::move(key)), value(std::move(value)) {}
+        : key(std::move(key)), value(std::move(value)) {
+    }
 };
 
 struct StreamEntry {
-    std::string entry_id;
+    int64_t timestamp{};
+    int64_t sequence{};
     std::vector<FieldValuePair> fields;
 
     StreamEntry() noexcept = default;
 
-    StreamEntry(std::string entry_id, std::string key, std::string value) noexcept
-        : entry_id(std::move(entry_id)) {
+    StreamEntry(const int64_t timestamp, const int64_t sequence, std::string key, std::string value)
+        : timestamp(timestamp), sequence(sequence) {
         fields.emplace_back(std::move(key), std::move(value));
     }
 };
 
 class RedisStream {
 public:
-    RedisStream(const RedisStream&) = delete;
-    RedisStream& operator=(const RedisStream&) = delete;
-    RedisStream(RedisStream&&) = delete;
-    RedisStream& operator=(RedisStream&&) = delete;
+    RedisStream(const RedisStream &) = delete;
+
+    RedisStream &operator=(const RedisStream &) = delete;
+
+    RedisStream(RedisStream &&) = delete;
+
+    RedisStream &operator=(RedisStream &&) = delete;
 
     static RedisStream &GetInstance() {
         static RedisStream instance;
         return instance;
     }
 
-    std::string Add(const std::string& stream_key, const std::string &entry_id, const std::string &key, const std::string &value);
-    bool ExistStreamKey(const std::string& stream_key) const;
+    std::string Add(const std::string &stream_key, const std::string &entry_id, const std::string &key,
+                    const std::string &value);
+
+    bool ExistStreamKey(const std::string &stream_key) const;
 
 private:
     RedisStream() = default;
     ~RedisStream() = default;
-    bool Exists(const std::string &stream_key, const std::string &entry_id);
-    std::unordered_map<std::string, StreamEntry> stream_entries_{};
+
+    std::unordered_map<std::string, std::vector<StreamEntry> > stream_entries_{};
 };
