7072d5aecbe3b2814f873b8bee9a7b7b33cb51cb
thuongnv2 <thuongnv2@sapo.vn>
2025-04-19 00:15:08 +0700

    Partially auto-generated IDs #yh3

--- src/database/RedisStream.cpp
+++ src/database/RedisStream.cpp
@@ -6,7 +6,6 @@ std::string RedisStream::Add(const std::string &stream_key,
                              const std::string &entry_id,
                              const std::string &key,
                              const std::string &value) {
-
     const auto pos = entry_id.find('-');
     if (pos == std::string::npos) {
         return RespParser::ToError("ERR wrong format of entry_id");
@@ -15,29 +14,45 @@ std::string RedisStream::Add(const std::string &stream_key,
     const std::string timestamp_str = entry_id.substr(0, pos);
     const std::string sequence_str = entry_id.substr(pos + 1);
 
-    if (!Utils::IsNumeric(timestamp_str) || !Utils::IsNumeric(sequence_str)) {
+    if (!Utils::IsNumeric(timestamp_str)) {
         return RespParser::ToError("ERR wrong number of arguments for 'xadd' command");
     }
 
     const int64_t new_timestamp = std::stoll(timestamp_str);
-    const int64_t new_sequence = std::stoll(sequence_str);
+    int64_t new_sequence{0};
 
-    if (new_timestamp == 0 && new_sequence == 0) {
-        return RespParser::ToError("ERR The ID specified in XADD must be greater than 0-0");
-    }
-
-    auto add_stream = [&]()-> std::string {
+    auto add_new_stream = [&]()-> std::string {
         const StreamEntry new_entry{new_timestamp, new_sequence, key, value};
         stream_entries_[stream_key].push_back(new_entry);
-        return RespParser::ToBulkString(entry_id);
+        const std::string new_entry_id = std::to_string(new_timestamp) + "-" + std::to_string(new_sequence);
+        return RespParser::ToBulkString(new_entry_id);
     };
 
     if (!stream_entries_.contains(stream_key)) {
-        return add_stream();
+        if (sequence_str == "*") {
+            new_sequence = new_timestamp == 0 ? 1 : 0;
+        } else if (Utils::IsNumeric(sequence_str)) {
+            new_sequence = std::stoll(sequence_str);
+        } else {
+            return RespParser::ToError("ERR wrong number of arguments for 'xadd' command");
+        }
+        return add_new_stream();
     }
 
     const std::vector<StreamEntry> &entries = stream_entries_[stream_key];
     const StreamEntry last_entry = entries.back();
+
+    if (sequence_str == "*") {
+        new_sequence = last_entry.timestamp == new_timestamp ? last_entry.sequence + 1 : 0;
+    } else if (Utils::IsNumeric(sequence_str)) {
+        new_sequence = std::stoll(sequence_str);
+    } else {
+        return RespParser::ToError("ERR wrong number of arguments for 'xadd' command");
+    }
+
+    if (new_timestamp == 0 && new_sequence == 0) {
+        return RespParser::ToError("ERR The ID specified in XADD must be greater than 0-0");
+    }
 
     const bool is_valid_sequence = new_timestamp > last_entry.timestamp
                                    || (new_timestamp == last_entry.timestamp && new_sequence > last_entry.sequence);
@@ -45,7 +60,7 @@ std::string RedisStream::Add(const std::string &stream_key,
     if (!is_valid_sequence) {
         return RespParser::ToError("ERR The ID specified in XADD is equal or smaller than the target stream top item");
     }
-    return add_stream();
+    return add_new_stream();
 }
 
 bool RedisStream::ExistStreamKey(const std::string &stream_key) const {
